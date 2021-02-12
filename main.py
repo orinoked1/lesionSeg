348e04bdf9fd12a846f3a9459eb7a54a047c2f52
galaviram <gal@htbioimaging.com>
2021-02-12 19:29:47 +0200

    Add loading trained models

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 
+
 # set flags / seeds
 torch.backends.cudnn.benchmark = True
 np.random.seed(1)
 torch.manual_seed(1)
 torch.cuda.manual_seed(1)
+
+PATH_TO_IMAGES = r'C:\Users\gal\Documents\Gal\University\Shitut\Project\images'#'D:\downloads\neuralNetwork',
+PATH_TO_CHECKPOINT = r'C:\Users\gal\Documents\Gal\University\Shitut\Project\models\weights'#'cpp_path/',
 
 # Start with main code
 if __name__ == '__main__':
@@ -20,7 +24,7 @@ if __name__ == '__main__':
         'shear_range': (0,0.3,0,0.3),
         'horizontal_flip_p': 0.5,
         'vertical_flip_p': 0.5,
-        'root_dir': r'D:\downloads\neuralNetwork',
+        'root_dir': PATH_TO_IMAGES,
         'csv_file': 'labels_lesion.csv',
         'batch_size': 8,
         'encoder_name': 'resnet18',
@@ -33,23 +37,22 @@ if __name__ == '__main__':
         verbose=True,
         monitor='val_loss',
         mode='min', ## for val_loss this should be min
-        dirpath='cpp_path/',
+        dirpath=PATH_TO_CHECKPOINT,
         filename='lesion-{epoch:02d}-{val_loss:.2f}'
     )
-    logger = TensorBoardLogger('tb_logs', name='my_model')
 
-    model = LesionModel(config)
+    logger = TensorBoardLogger('tb_logs', name='my_model')
     data_module = LesionDataModule(config)
-    # Load_model = 1 ## TODO: Make this work
-    # if Load_model:
-    #     checkpoint = torch.load(r'C:\Users\gal\Documents\Gal\University\Shitut\Project\models\weights.ckpt',
-    #                             map_location=lambda storage, loc: storage)
-    #     exp = Experiment(model)
-    #     exp.load_state_dict(checkpoint['state_dict'])
-    #     trainer = pl.Trainer(experiment=exp)
-    # else:
+    model = LesionModel(config)
+
+    Load_model = 1
+    if Load_model:
+        checkpoint = torch.load(PATH_TO_CHECKPOINT + '.ckpt')
+        model.load_state_dict(checkpoint['state_dict'])
+
     trainer = pl.Trainer(gpus=1, max_epochs=300, progress_bar_refresh_rate=20, automatic_optimization=True,
-                             logger=logger, checkpoint_callback=[checkpoint_callback])
+                         logger=logger, checkpoint_callback=[checkpoint_callback])
+
     trainer.fit(model, data_module)
 
 
